1f041a2f1f860858b3524793cc670229643971fa
GISSANDRO M DANTAS GAMA <gissandrogama@gmail.com>
2021-01-13 01:18:33 -0300

    Ci and cd (#3)
    * start of travis configuration
    
    * downgrade otp
    
    * correction credo
    
    * corection travis
    
    * add fixiture post, tests of view post, session and controller
    
    * modifi
    
    * modify posts
    
    * modify
    
    * modify
    
    * shields
    
    * shields
    
    * gigalixir cd process

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+echo "Start Gigalixir Deploy"
+git remote add gigalixir https://yc_lord%40hotmail.com:e84fdb31-958a-4b2c-b4ca-c5dff4331106@git.gigalixir.com/blog-api.git
+git push gigalixir master
+echo "Finish Gigalixir Deploy"
